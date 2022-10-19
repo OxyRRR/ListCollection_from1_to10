07bf88344f261692b43781efb846fb478450b713
OxyRRR <ksenijarepina230488@gmail.com>
2022-10-19 22:12:38 +0500

    !!!

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+
+    val list = mutableListOf<Int>()
+
+    for (index in 1..10) {
+        list.add(index)
+    }
+    print(list)
+}
+    fun printList(list: ArrayList<Int>) {
+        print(list)
+    }
